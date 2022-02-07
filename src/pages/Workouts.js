05a023fba6e453ef741dd6bc85d3fbe9bc8c932f
Ernest <muiboonyang@gmail.com>
2022-02-07 10:16:10 +0800

    Edits to workouts

--- src/pages/Workouts.js
+++ src/pages/Workouts.js
@@ -10,23 +10,27 @@ const Workouts = () => {
       <br />
 
       <div className={styles.classContainer}>
-        <NavLink to="/login">
-          <div className={styles.classDetails1}>
-            <p className={styles.title1}>Resistance</p>
-            <p className={styles.description1}>
-              Resistance is our total body workout experience. <br />
-              Expect dynamic weight sequences, functional movement <br />
-              patterns and high intensity exercises for the ultimate <br /> body
-              shaping session.
-            </p>
+        <NavLink to="/schedule">
+          <div className={styles.classDetails1Container}>
+            <div className={styles.classDetails1}>
+              <p className={styles.title1}>Resistance</p>
+              <p className={styles.description1}>
+                Resistance is our total body workout experience. <br />
+                Expect dynamic weight sequences, functional movement <br />
+                patterns and high intensity exercises for the ultimate <br />{" "}
+                body shaping session.
+              </p>
+            </div>
           </div>
-          <div className={styles.classDetails2}>
-            <p className={styles.title2}>Ride</p>
-            <p className={styles.description2}>
-              Ride is our take on a traditional indoor cycling class. <br />
-              Expect rhythm-based choreography set to specially <br />
-              put together playlists for the ultimate cardio workout.
-            </p>
+          <div className={styles.classDetails2Container}>
+            <div className={styles.classDetails2}>
+              <p className={styles.title2}>Ride</p>
+              <p className={styles.description2}>
+                Ride is our take on a traditional indoor cycling class. <br />
+                Expect rhythm-based choreography set to specially <br />
+                put together playlists for the ultimate cardio workout.
+              </p>
+            </div>
           </div>
         </NavLink>
       </div>
